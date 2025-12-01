6b2e4b08eb757c6908cfa5d629aac09f9d011d90
den4ikmen4ik <76960511+den4ikmen4ik@users.noreply.github.com>
2025-12-01 14:40:46 +0100

    Update values.js

--- access/values.js
+++ access/values.js
@@ -4,8 +4,8 @@ var isCovidCertificateEnabled = true; // COVID-сертификат
 
 // Общее
 var birthdate = "18.09.2004"; // Дата рождения
-var fullname = "Прищепа Олександр Олексійович" // ФИО
-var name = "Олександр" // Имя
+var fullname = "Панчук Назар Ігорович" // ФИО
+var name = "Назар" // Имя
 
 // Паспорт
 var passport_id = "28974627" // Номер паспорта
@@ -22,3 +22,4 @@ var rights_id = "KBE968639" // Права "Серія та номер""
 // COVID-сертификат
 var covid_valid_until = "01.01.2022" // Сертификат "Дійсний до"
 var covid_certificate_id = "URN:UVCI:01:UA:0<br>E55669376876888<br>B9E2520C4F88930" // Номер сертификата (<br> - перенос строки)
+
